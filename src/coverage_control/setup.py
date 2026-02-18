edecac4533506d8745df80a0698551d3a1c6575b
ee250002034-eng <ee250002034@iiti.ac.in>
2026-02-18 18:43:33 +0530

    Create setup.py

---
+++ src/coverage_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup 
+ 
+package_name = 'coverage_control' 
+ 
+setup( 
+    name=package_name, 
+    version='0.0.0', 
+    packages=[package_name], 
+    data_files=[ 
+        ('share/ament_index/resource_index/packages', 
+         ['resource/' + package_name]), 
+        ('share/' + package_name, ['package.xml']), 
+    ], 
+    install_requires=['setuptools'], 
+    zip_safe=True, 
+    maintainer='you', 
+    maintainer_email='you@example.com', 
+    description='Adaptive Voronoi coverage controller', 
+    license='Apache License 2.0', 
+    entry_points={ 
+        'console_scripts': [ 
+            'adaptive_controller = coverage_control.adaptive_controller:main', 
+        ], 
+    }, 
+) 
+ 
